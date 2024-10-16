59c883bbe918e7f764373e931178c997554af821
nastyaan <nastya200458@gmail.com>
2024-10-16 04:36:19 +0300

    DB migration + registration form

--- MyScatterPlotApp/Controllers/AccountController.cs
+++ MyScatterPlotApp/Controllers/AccountController.cs
@@ -2,69 +2,72 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
-public class AccountController : Controller
+namespace MyScatterPlotApp.Models
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
-
-    public AccountController(UserManager<ApplicationUser> userManager,
-                             SignInManager<ApplicationUser> signInManager)
+    public class AccountController : Controller
     {
-        _userManager = userManager;
-        _signInManager = signInManager;
-    }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-    [HttpGet]
-    public IActionResult Register() => View();
-
-    [HttpPost]
-    public async Task<IActionResult> Register(RegisterViewModel model)
-    {
-        if (ModelState.IsValid)
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager)
         {
-            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-            var result = await _userManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
-            {
-                await _signInManager.SignInAsync(user, isPersistent: false);
-                return RedirectToAction("Index", "Chart");
-            }
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
-        return View(model);
-    }
 
-    [HttpGet]
-    public IActionResult Login() => View();
+        [HttpGet]
+        public IActionResult Register() => View();
 
-    [HttpPost]
-    public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
-    {
-        if (ModelState.IsValid)
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            var result = await _signInManager.PasswordSignInAsync(model.Email,
-                                                                  model.Password,
-                                                                  model.RememberMe,
-                                                                  lockoutOnFailure: false);
-            if (result.Succeeded)
+            if (ModelState.IsValid)
             {
-                if (string.IsNullOrEmpty(returnUrl))
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("Index", "Chart");
-                else
-                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
-            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
         }
-        return View(model);
-    }
 
-    [HttpPost]
-    public async Task<IActionResult> Logout()
-    {
-        await _signInManager.SignOutAsync();
-        return RedirectToAction("Login", "Account");
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email,
+                                                                      model.Password,
+                                                                      model.RememberMe,
+                                                                      lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(returnUrl))
+                        return RedirectToAction("Index", "Chart");
+                    else
+                        return LocalRedirect(returnUrl);
+                }
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
     }
 }
--- MyScatterPlotApp/Models/LoginViewModel.cs
+++ MyScatterPlotApp/Models/LoginViewModel.cs
@@ -1,17 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class LoginViewModel
+namespace MyScatterPlotApp.Models
 {
-    [Required]
-    [EmailAddress]
-    [Display(Name = "Email")]
-    public string Email { get; set; }
 
-    [Required]
-    [DataType(DataType.Password)]
-    [Display(Name = "Password")]
-    public string Password { get; set; }
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
-    [Display(Name = "Remember me?")]
-    public bool RememberMe { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
 }
