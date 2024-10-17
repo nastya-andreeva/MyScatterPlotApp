c1f437cd80528fd5290b5739278ffee8be5751c4
nastyaan <nastya200458@gmail.com>
2024-10-17 18:14:35 +0300

    when exiting, redirects to the login page

--- MyScatterPlotApp/Controllers/AccountController.cs
+++ MyScatterPlotApp/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace MyScatterPlotApp.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
     }
 }
